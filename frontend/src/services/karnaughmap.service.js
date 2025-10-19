f65ebe25605ec9255fb40b7ffd5687c5e45ecbec
Erik Laki <erik.laki@student.tuke.sk>
2025-10-19 21:44:51 +0200

    asdsadsad fgixed

--- frontend/src/services/karnaughmap.service.js
+++ frontend/src/services/karnaughmap.service.js
@@ -1,5 +1,4 @@
-const API_URL = "http://localhost:3001/api";
-
+const API_URL = import.meta.env.VITE_API_URL;
 export const karnaughMapService = {
   async saveKM(data) {
     const response = await fetch(`${API_URL}/karnaughmap`, {
